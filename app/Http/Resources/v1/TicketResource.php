5ee103fbdb3b736e36bfe143f3268515546a9850
Takezo316 <takezo316@gmail.com>
2024-08-08 18:46:52 -0500

    Beta

---
+++ app/Http/Resources/v1/TicketResource.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Resources\v1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TicketResource extends JsonResource
+{
+    //public static $wrap = 'ticket';
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        //Payload Design
+        return [
+            'type' => 'ticket',
+            'id' => $this->id,
+            'attibutes' => [
+                'title' => $this->title,
+                'description' => $this->when(
+                    $request->routeIs('tickets.show'),
+                    $this->description
+                ),
+                'status' => $this->status,
+                'createdAt' => $this->created_at,
+                'updatedAt' => $this->updated_at
+            ],
+            'relationships' => [
+                'author' => [
+                    'data' => [
+                        'type' => 'user',
+                        'id' => $this->user_id,
+                    ],
+                    'links' => [
+                        'self' => route('users.show', ['user' => $this->user_id])
+                    ]
+                ]
+            ],
+            'includes' => new UserResource($this->whenLoaded('author')),
+            'links' => [
+                'self' => route('tickets.show', [$this->id])
+            ]
+        ];
+    }
+}
---
+++ app/Http/Resources/v1/UserResource.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Resources\v1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'user',
+            'id' => $this->id,
+            'attributes' => [
+                'name' => $this->name,
+                'email' => $this->email,
+                //Form 1
+                /*'emailVerifiedAt' => $this->when(
+                    $request->routeIs('users.*'),
+                    $this->email_verified_at
+                )*/
+                //Form 2
+                $this->mergeWhen($request->routeIs('users.*'),[
+                    'emailVerifiedAt' => $this->email_verified_at,
+                    'createdAt' => $this->created_at,
+                    'updatedAt' => $this->updated_at,
+                ])
+            ],
+            'includes' => TicketResource::collection($this->whenLoaded('tickets')),
+            'links' => [
+                'self' => route('users.show', [$this->id])
+            ]
+        ];
+    }
+}
